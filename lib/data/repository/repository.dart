12e7e30d77e5e0d435a68b89caedb6e2ed8b1a73
NguyenDucThanhNhan <ngducthanhnhan@gmail.com>
2024-08-15 22:23:31 +0700

    Read JSON content from Internet

---
+++ lib/data/repository/repository.dart
@@ -0,0 +1,30 @@
+import 'package:music_app/data/source/source.dart';
+
+import '../model/song.dart';
+
+abstract interface class Repository {
+  Future<List<Song>?> loadData();
+}
+
+class DefaultRepository implements Repository {
+  final _localDataSource = LocalDataSource();
+  final _remoteDataSource = RemoteDataSource();
+  @override
+  Future<List<Song>?> loadData() async {
+    List<Song> songs = [];
+    await _remoteDataSource.loadData().then((remoteSong) {
+      if (remoteSong == null) {
+        _localDataSource.loadData().then((localSong) {
+          if (localSong!= null) {
+            songs.addAll(localSong);
+          }
+        });
+      } else {
+        songs.addAll(remoteSong);
+      }
+    });
+
+    return songs;
+  }
+
+}
